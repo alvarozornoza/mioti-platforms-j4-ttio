c13fc987644d2957b2b16eb02ea0e6a9760c95e4
Alvaro Zornoza <azuzornoz@gmail.com>
2022-11-06 14:12:26 +0100

    New stuff

--- ttio_exportdata.py
+++ ttio_exportdata.py
@@ -3,8 +3,6 @@ import requests
 import json
 
 THING_TOKEN = ''
-RESOURCE_KEY = 'temperature'
-LIMIT = 100
 START_DATE = '2022-01-01T00:00:00.000Z'
 END_DATE = '2022-12-31T23:59:59.999Z'
 
@@ -16,7 +14,7 @@ def getResourceKeys():
     return parsed['resources']
 
 def getResourceValues(key):
-    url = f'https://api.thethings.io/v2/things/{THING_TOKEN}/resources/{key}?limit={LIMIT}&startDate={START_DATE}&endDate={END_DATE}'
+    url = f'https://api.thethings.io/v2/things/{THING_TOKEN}/resources/{key}?&startDate={START_DATE}&endDate={END_DATE}'
     headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers)
     parsed = json.loads(response.text)
