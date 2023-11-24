f99c35a0797613c0664052ce3e4f264ab54c9140
Alvaro Zornoza <alvaro.zornoza@gmail.com>
2023-11-24 14:16:15 +0100

    Update year

--- http/ttio_http_thing_read.py
+++ http/ttio_http_thing_read.py
@@ -4,8 +4,8 @@ import json
 THING_TOKEN = ''
 RESOURCE_KEY = 'temperature'
 LIMIT = 100
-START_DATE = '2022-01-01T00:00:00.000Z'
-END_DATE = '2022-12-31T23:59:59.999Z'
+START_DATE = '2023-01-01T00:00:00.000Z'
+END_DATE = '2023-12-31T23:59:59.999Z'
 
 url = f'https://api.thethings.io/v2/things/{THING_TOKEN}/resources/{RESOURCE_KEY}?limit={LIMIT}&startDate={START_DATE}&endDate={END_DATE}'
 
